1fc85d0b7cbd5bc0ba40bf20cfaa13718d951c63
Ioannis Monastiridis <mongiannis@gmail.com>
2026-05-13 06:38:43 +0000

    feat: add NSG with deny rule to networking module

--- Terraform_Azure_AGW_VM/modules/networking/main.tf
+++ Terraform_Azure_AGW_VM/modules/networking/main.tf
@@ -25,3 +25,26 @@ resource "azurerm_subnet" "bastion_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-p44010"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  security_rule {
+    name                       = "deny-specific-ip"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_address_prefix      = "203.0.113.99/32"
+    destination_address_prefix = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "vm_subnet_nsg" {
+  subnet_id                 = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
